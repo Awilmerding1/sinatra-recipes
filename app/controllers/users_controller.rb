2b9364a2c2eaed258f880b5b72dfa3a07bfbbf65
Annabel Wilmerding <awilmerding@NYC-US16685-MBP.local>
2020-03-24 18:59:50 -0400

    signup, login, validations, user show page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,41 @@
 class UsersController < ApplicationController
 
 
+  get '/signup' do
+    erb :'users/signup'
+  end
+
+  get '/login' do
+    erb :'users/login'
+  end
+
+  post '/login' do
+    user = User.find_by(username: params[:username])
+    if user && user.authenticate(params[:password])
+      session[:user_id] = user.id
+      redirect to "/users/#{user.id}"
+    else
+      redirect to '/signup'
+    end
+  end
+
+  post '/signup' do
+    user = User.create(params)
+    if user.valid?
+      session[:user_id] = user.id
+      redirect to "/users/#{user.id}"
+    else
+      redirect to '/signup'
+    end
+  end
+
+  get '/users/:id' do
+    if User.find_by(id: params[:id])
+      @user = User.find_by(id: params[:id])
+    else
+      redirect to '/'
+    end
+    erb :'users/show'
+  end
 
 end
